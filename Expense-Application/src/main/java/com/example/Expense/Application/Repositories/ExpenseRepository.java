0f6bd3f689b1e2e7cd99a9b644c3385269bba0d9
MohanReddy <mohanreddyderedla148@gmail.com>
2024-10-14 19:24:01 +0530

    comments added

--- Expense-Application/src/main/java/com/example/Expense/Application/Repositories/ExpenseRepository.java
+++ Expense-Application/src/main/java/com/example/Expense/Application/Repositories/ExpenseRepository.java
@@ -1,9 +1,13 @@
 package com.example.Expense.Application.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.Expense.Application.Models.Expense;
-
+/**
+ * Repository interface for the Expense entity.
+ */
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer>{
 
 }
--- Expense-Application/src/main/java/com/example/Expense/Application/Repositories/UserRepository.java
+++ Expense-Application/src/main/java/com/example/Expense/Application/Repositories/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Expense.Application.Models.User;
-
+/**
+ * Repository interface for the User entity.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
